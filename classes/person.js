ff8d7a6f933881d992a738f11aeb90ad250c5b5a
Benji <64211264+GrumpyBenji@users.noreply.github.com>
2022-12-10 06:21:00 -0800

    Solved

--- classes/person.js
+++ classes/person.js
@@ -1,4 +1,38 @@
 // Your code here
+class Person {
+  constructor(firstName, lastName, age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+
+  introduce(){
+    console.log(`Hi, I'm ${this.firstName} ${this.lastName}, and I'm ${this.age} years old.`);
+    return;
+  }
+
+  static introducePeople(array){
+    let validArray = true;
+
+    if(!(array instanceof Array)){
+      throw new Error('introducePeople only takes an array as an argument.');
+    }
+
+    array.forEach(element => {
+      if(!(element instanceof Person)){
+        validArray = false;
+        throw new Error('All items in array must be Person class instances.');
+      }
+    }
+  )
+  if(validArray === true ){
+      array.forEach(Person => {
+        Person.introduce(); 
+          }
+        )
+      }
+  }  
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
